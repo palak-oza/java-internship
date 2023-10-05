381b5dd589a21ee6bd0c1f41d91c72d1196c3ea4
Palak Oza <86180372+palak-oza@users.noreply.github.com>
2023-10-06 01:05:43 +0530

    Update ArrayManipulation.java

--- ArrayManipulation.java
+++ ArrayManipulation.java
@@ -1,3 +1,15 @@
+//Wrong java code
+/*public class ArrayManipulation {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+        for (int i = 0; i <= numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}*/
+
+//Rectified code
 public class ArrayManipulation {
     public static void main(String[] args) {
         int[] numbers ={1, 2, 3, 4, 5};
